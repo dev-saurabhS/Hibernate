678619b4522e44862eb4ea270941b15576c0e6c5
saurabh satpathi <ssatpathi95@gmail.com>
2022-09-26 23:50:59 +0530

    school-app-hibernate added

---
+++ school-app-hibernate/src/main/java/com/ty/school/dto/User.java
@@ -0,0 +1,59 @@
+package com.ty.school.dto;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table(name ="school_app_hibernate")
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	@Column (name="User_email", unique = true)
+	private String email;
+	@Column (name = "user_password", nullable = false)
+	private String password;
+	@Column (name = "user_phone", unique = true)
+	private long phone;
+	@Column (name = "user_gender", nullable = false)
+	private String gender;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public long getPhone() {
+		return phone;
+	}
+	public void setPhone(long phone) {
+		this.phone = phone;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	
+	
+}
